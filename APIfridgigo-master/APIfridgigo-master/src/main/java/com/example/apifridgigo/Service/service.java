38f712a3bea1028eb05a79fab3b24e8275e647f2
marstrash <82026246+marstrash@users.noreply.github.com>
2022-02-24 01:19:29 +0100

    Add files via upload

--- APIfridgigo-master/APIfridgigo-master/src/main/java/com/example/apifridgigo/Service/service.java
+++ APIfridgigo-master/APIfridgigo-master/src/main/java/com/example/apifridgigo/Service/service.java
@@ -1,34 +1,22 @@
 package com.example.apifridgigo.Service;
 
-public class service {
-    
-}
-
-
-import com.example.apifridgigo.Entity.Rezepte.Rezept;
-import com.example.demo.model.Rezepte;
-import com.example.demo.repository.UserRepository;
+import com.example.apifridgigo.Repository.RezeptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
+
 @Service
 @Transactional
 public class RezeptService {
+
     @Autowired
-    private RezeptRepository rezeptRepository;
-    public List<Rezept> listAllRezepts() {
-        return rezept-Repository.findAll();
+    private RezeptRepository repository;
+
+    public List<Rezept> saveRezepte(List<Rezept> rezepte){
+       return repository.saveAll(rezepte);
     }
 
-    public void saveRezepts(Rezept rezept) {
-        rezeptRepository.save(rezept);
-    }
+    
 
-    public User getUser(Integer id) {
-        return rezeptRepository.findById(id).get();
-    }
 
-    public void deleteUser(Integer id) {
-        rezeptRepository.deleteById(id);
-    }
 }
